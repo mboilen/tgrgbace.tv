018ea87d02a2ce7b0eb836ca10038f217fb7818d
Mike Boilen <mgb@alumni.brown.edu>
2021-06-01 04:19:03 +0000

    Add CryptoJS to webpack and shuffle code to have index.js to decryption

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+var CryptoJS = require("crypto-js");
 function component() {
+    var key = new URLSearchParams(window.location.search).get('key');
+    var sourcesString = getSourcesString();
+    var decrypted = CryptoJS.AES.decrypt(sourcesString, key).toString(CryptoJS.enc.Utf8);
+    var sourcesList = JSON.parse(decrypted);
     var player = OvenPlayer.create("player", {
-        sources: getSources(new URLSearchParams(window.location.search).get('key'))
+        sources: sourcesList
     });
 }
 component();

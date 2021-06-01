045298be8edcf759e6ee47c18be8ff10c755d51e
Mike Boilen <mgb@alumni.brown.edu>
2021-06-01 03:49:21 +0000

    Now works with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import _ from 'ovenplayer'
 function component() {
     var player = OvenPlayer.create("player", {
         sources: getSources(new URLSearchParams(window.location.search).get('key'))
     });
 }
+component();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'TGRGBACE Live!',
+            template: 'src/index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'node_modules/ovenplayer/dist/*.js',
+                    to: 'ovenplayer/[name][ext]' },
+                { from: 'node_modules/hls.js/dist/hls.min.js',
+                    to: 'deps/[name][ext]' },
+                { from: 'node_modules/dashjs/dist/dash.all.min.js',
+                    to: 'deps/[name][ext]' }
+            ]
+        }),
+    ],
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
 };

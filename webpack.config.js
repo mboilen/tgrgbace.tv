a53bb7d35261c54e0bf07ec3aabd84d6e0c15669
Mike Boilen <mgb@alumni.brown.edu>
2021-06-01 03:50:27 +0000

    Switch to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
